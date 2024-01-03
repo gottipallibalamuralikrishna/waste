f0b31ea69c197b720e7b0722d509d5ca77227dc5
Bala Murali K. Gottipalli <bgottipalli@fulcrumgt.com>
2024-01-03 18:08:57 +0530

    waste it is not important

---
+++ App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import { Inc } from './hm';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello React user</h1>
+      <Inc></Inc>
+    </div>
+  );
+}
+
+export default App;
---
+++ hm.js
@@ -0,0 +1,31 @@
+import { useState, useEffect, Component } from "react";
+import { Button, Fab, TextField } from "@mui/material";
+import MovieIcon from "@mui/icons-material/Movie";
+
+import styles from "./style.module.css";
+
+function Inc() {
+  const [name, setName] = useState("");
+
+  useEffect(() => {
+    document.title = `the name is ${name}`;
+  }, [name]);
+
+  return (
+    <div>
+        <MovieIcon fontSize="large" />
+        <TextField
+            className={styles.textfield}
+            type="text"
+            placeholder="enter the name"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+            size="small"
+            style={{ paddingRight: 10 }}>
+        </TextField>
+        <Button variant="contained">add name</Button>
+        <Fab variant="circular" color="success"></Fab>
+    </div>
+  );
+}
+export { Inc };
